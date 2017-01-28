8634163690e7221366eee9c3679a320b5fffcc42
agarzonp <agarzonp@gmail.com>
2017-01-28 01:30:22 +0100

    Initialise GLFW and GLEW, create the window and a basic main loop

--- SplineCam/src/main.cpp
+++ SplineCam/src/main.cpp
@@ -6,11 +6,48 @@
 
 int main()
 {
+	// init GLFW and GLEW
 	if (!glfwInit())
 	{
 		std::cerr << "GLFW init failed!" << std::endl;
 		return -1;
 	}
+
+	glewExperimental = GL_TRUE;
+
+	if (!glewInit())
+	{
+		std::cerr << "GLEW init failed!" << std::endl;
+		return -1;
+	}
+
+	// set window configuration
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // only support modern opengl
+
+	// create the window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "SplineCam", nullptr, nullptr);
+	if (!window)
+	{
+		std::cerr << "GLFW failed to create the window!" << std::endl;
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	// main loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
