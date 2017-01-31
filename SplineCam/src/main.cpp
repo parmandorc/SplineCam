198f2350c99cad1d23afb68600c5525687acbd4c
agarzonp <agarzonp@gmail.com>
2017-02-01 00:08:39 +0100

    enable depth test

--- SplineCam/src/main.cpp
+++ SplineCam/src/main.cpp
@@ -40,6 +40,8 @@ int main()
 		return -1;
 	}
 
+	glEnable(GL_DEPTH_TEST);
+
 	// init SplineCam
 	SplineCam splineCam;
 
@@ -57,7 +59,7 @@ int main()
 		glfwPollEvents();
 
 		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		splineCam.Update();
 		splineCam.Render();
