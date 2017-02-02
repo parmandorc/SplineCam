c5556e0beff865ef474cec75444442e3a01e4c37
Pablo Armando Rodríguez Codes <parmandorc@gmail.com>
2017-02-02 13:33:40 +0100

    Added simple spline calculation
    For now, spline is calculated as a cubic bezier curve of 4 control
    points

--- SplineCam/src/SplineCam/Spline/Spline.h
+++ SplineCam/src/SplineCam/Spline/Spline.h
@@ -25,7 +25,16 @@ public:
 	}
 
 	std::vector<glm::vec3> getSplinePoints() {
-		return controlPoints;
+		std::vector<glm::vec3> points;
+		for (float t = 0.0f; t <= 1.0f; t += 0.025f) {
+			points.push_back(
+				controlPoints[0] * (-t * t * t + 3 * t * t - 3 * t + 1) +
+				controlPoints[1] * (3 * t * t * t - 6 * t * t + 3 * t) +
+				controlPoints[2] * (-3 * t * t * t + 3 * t * t) +
+				controlPoints[3] * (t * t * t)
+			);
+		}
+		return points;
 	}
 
 protected:
